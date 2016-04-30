33492718b2ab2c742ea125cb0fd479278f839ae0
Ben Mather <bwhmather@bwhmather.com>
2016-04-30 16:54:29 +0100

    Split up into separate modules

---
+++ src/layout.rs
@@ -0,0 +1,152 @@
+use std::fmt::Debug;
+use std::mem;
+
+use leonardo::leonardo;
+use subheap::SubHeapMut;
+
+
+#[derive(Clone, Debug)]
+pub struct Layout {
+    orders: u64,
+    size: usize,
+}
+
+
+impl Layout {
+    pub fn new() -> Self {
+        Layout {
+            orders: 0,
+            size: 0,
+        }
+    }
+
+    pub fn new_from_len(size: usize) -> Self {
+        let mut orders = 0;
+        let mut remaining = size;
+
+        for order in (0..63).rev() {
+            if leonardo(order) <= remaining {
+                remaining -= leonardo(order);
+                orders |= 1 << order;
+            }
+        }
+
+        Layout {
+            orders: orders,
+            size: size,
+        }
+    }
+
+    pub fn push(&mut self) {
+        self.size += 1;
+
+        match self.lowest_order() {
+            Some(lowest_order) => {
+                let mergeable_mask : u64 = 3 << lowest_order;
+
+                if (mergeable_mask & self.orders) == mergeable_mask {
+                    // lowest two sub-heaps are adjacent and car be merged
+                    // Clear the two lowest orders
+                    self.orders &= !mergeable_mask;
+
+                    // Replace them with the next order up
+                    self.orders |= 1 << (lowest_order + 2);
+                } else if lowest_order == 1 {
+                    self.orders |= 1;
+                } else {
+                    self.orders |= 2;
+                }
+            }
+            None => {
+                self.orders |= 2;
+            }
+        }
+    }
+
+    pub fn pop(&mut self) {
+        self.size -= 1;
+
+        match self.lowest_order() {
+            Some(lowest_order) => {
+                // Clear the order
+                let mask : u64 = 1 << lowest_order;
+                self.orders &= !mask;
+
+                // If the order is not zero or one (the single element orders)
+                // then we need to split it into two heaps of size n-1 and n-2
+                if lowest_order != 0 && lowest_order != 1 {
+                    let mask : u64 = 3 << lowest_order - 2;
+                    self.orders |= mask;
+                }
+            }
+            None => {}
+        }
+    }
+
+    #[inline]
+    pub fn lowest_order(&self) -> Option<u32> {
+        match self.orders.trailing_zeros() {
+            64 => None,
+            n => Some(n),
+        }
+    }
+
+    pub fn iter<'a, T : Ord + Debug>(&self, data : &'a mut [T]) -> SubHeapIterMut<'a, T> {
+        assert_eq!(data.len(), self.size);
+
+        SubHeapIterMut {
+            heap: data,
+            orders: self.orders,
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub struct SubHeapIterMut<'a, T: 'a> {
+    heap: &'a mut [T],
+    orders: u64,
+}
+
+
+impl<'a, T : Ord + Debug> Iterator for SubHeapIterMut<'a, T>
+{
+    type Item = SubHeapMut<'a, T>;
+
+    fn next(&mut self) -> Option<SubHeapMut<'a, T>> {
+        if self.orders != 0 {
+            // Records and remove the first order from the font of the bitset
+            // This is the order of the sub-heap at the start of the heap
+            let order = self.orders.trailing_zeros();
+            self.orders ^= 1 << order;
+
+            // We need to pre-calculate the length to get around the fact that
+            // the borrow checker can't yet handle borrowing in for only as
+            // long as is needed to calculate the argument to a function
+            let heap_len = self.heap.len();
+
+            // In order to avoid having more than one mutable reference to the
+            // heap at any one time,we have to temporarily replace it in self
+            // with a placeholder value.
+            let mut heap_data = mem::replace(&mut self.heap, &mut []);
+
+            // Split the heap into the part belonging to this sub-heap and all
+            // of the rest
+            let (mut rest_data, mut subheap_data) = heap_data.split_at_mut(
+                heap_len - leonardo(order)
+            );
+
+            // Store what's left of the heap back in self
+            self.heap = rest_data;
+
+            Some(SubHeapMut::new(subheap_data, order))
+        } else {
+            None
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let ones = self.orders.count_ones() as usize;
+        (ones, Some(ones))
+    }
+}
